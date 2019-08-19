7a6575b7040b5990f04dbf62f5104e1e231c85d4
raphaelvmiollo <rmiollo@inf.ufsm.br>
2019-08-19 18:30:40 -0300

    Tela de execução - primeira edição

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ns-app",
+    moduleId: module.id,
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
+
+})
+export class AppComponent { }
